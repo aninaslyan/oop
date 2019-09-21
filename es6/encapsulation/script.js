7adf6277c7740bc9d77ab0bf0a8ee9e4e9094db6
aninaslyan <ani.naslyan@gmail.com>
2019-09-21 14:16:51 +0400

    encapsulation es6, rather protected than private

---
+++ es6/encapsulation/script.js
@@ -0,0 +1,27 @@
+class Employee {
+    _salary = 0; // rather protected (_) than private (#)
+
+    getSalary() {
+        return this._salary;
+    }
+
+    setSalary(s) {
+        if(typeof s !== 'number') {
+            throw new Error("The value must be a number");
+        }
+        this._salary = s;
+    }
+}
+
+let e1 = new Employee();
+console.log(e1);
+
+class Developer extends Employee {
+    constructor(releases) {
+        super();
+        this.releases = releases;
+    }
+}
+
+let d1 = new Developer(1);
+console.log(d1);
